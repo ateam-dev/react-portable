e1e29a2da250f11914e854cfedbccb2554752c2c
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-05-10 18:04:15 +0900

    update mini

--- examples/mini/src/entry.tsx
+++ examples/mini/src/entry.tsx
@@ -4,11 +4,14 @@ import { Button } from "./Button";
 const Entry: FC<{ n?: number }> = ({ n }) => {
   const [counter, increment] = useReducer((s) => s + 1, n ?? 0);
   return (
-    <div>
-      <span className="text-5xl text-green-500">{counter}</span>
-      <Button className="text-5xl" onClick={increment}>
-        インクリメント！
+    <div className="p-2">
+      <Button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={increment}
+      >
+        +1
       </Button>
+      <span className="ml-3 text-xl text-green-500">{counter}</span>
     </div>
   );
 };
