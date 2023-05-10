9f04319c7bdddb1f70314014a417aa55f48991ac
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-05-10 18:04:05 +0900

    add workspace nextjs

---
+++ examples/nextjs/components/ReactPortable.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useEffect, useReducer } from "react";
+
+export const ReactPortable = ({
+  src,
+  force,
+}: {
+  src: string;
+  force?: boolean;
+}) => {
+  const [isMounted, mounted] = useReducer(() => true, false);
+  useEffect(() => {
+    mounted();
+  }, []);
+  if (force) return <react-portable src={src} />;
+  return <react-portable src={src} suspend={String(!mounted)} />;
+};
