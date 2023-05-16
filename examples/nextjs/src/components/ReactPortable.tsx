8d4bee1a5ab37bc3faf6858a2d480d160893677a
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-05-16 16:39:37 +0900

    update example

--- examples/nextjs/src/components/ReactPortable.tsx
+++ examples/nextjs/src/components/ReactPortable.tsx
@@ -3,14 +3,22 @@ import { useEffect, useReducer } from "react";
 export const ReactPortable = ({
   entry,
   force,
+  gateway,
 }: {
   entry: string;
   force?: boolean;
+  gateway?: string;
 }) => {
   const [isMounted, mounted] = useReducer(() => true, false);
   useEffect(() => {
     mounted();
   }, []);
-  if (force) return <react-portable entry={entry} />;
-  return <react-portable entry={entry} suspend={String(!isMounted)} />;
+  if (force) return <react-portable gateway={gateway} entry={entry} />;
+  return (
+    <react-portable
+      gateway={gateway}
+      entry={entry}
+      suspend={String(!isMounted)}
+    />
+  );
 };
