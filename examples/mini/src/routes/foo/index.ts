369c85e90c658ff9277dbaf7970d736625b91b90
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-05-16 15:03:15 +0900

    upadte examples

--- examples/mini/src/routes/foo/index.ts
+++ examples/mini/src/routes/foo/index.ts
@@ -1,15 +1,20 @@
 import Entry from "../../entry";
 export default Entry;
 import "../../global.css";
+import { Loader, Strategy } from "react-portable-core";
 
-export const loader = (request: Request) => {
+export const loader: Loader = (request: Request) => {
   const url = new URL(request.url);
   const n = url.searchParams.get("n");
 
   return { n: Number(n ?? "-100") };
 };
 
-export const option = {
-  eagerness: "hover",
-  event: ["focusin"],
-} as const;
+export const strategy: Strategy = {
+  cache: {
+    revalidate: 0,
+  },
+  activate: {
+    eagerness: "visible",
+  },
+};
--- examples/mini/src/routes/index.ts
+++ examples/mini/src/routes/index.ts
@@ -1,15 +1,21 @@
 import Entry from "../entry";
 export default Entry;
 import "../global.css";
+import { Loader, Strategy } from "react-portable-core/types";
 
-export const loader = (request: Request) => {
+export const loader: Loader = (request: Request) => {
   const url = new URL(request.url);
   const n = url.searchParams.get("n");
 
   return { n: Number(n ?? "0") };
 };
 
-export const option = {
-  eagerness: "hover",
-  event: ["focusin"],
-} as const;
+export const strategy: Strategy = {
+  cache: {
+    revalidate: 0,
+  },
+  activate: {
+    eagerness: "hover",
+    event: ["focusin"],
+  },
+};
