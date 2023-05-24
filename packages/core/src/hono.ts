9b5fc11c721b07a3829462494f8f96806c54dc87
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-05-24 12:06:02 +0900

    update core for etag handler

--- packages/core/src/hono.ts
+++ packages/core/src/hono.ts
@@ -1,26 +1,35 @@
 import qwikCityPlan from "@qwik-city-plan";
 import render from "./entry.ssr";
 import { qwikMiddleware } from "@hono/qwik-city";
-
 import { Hono } from "hono";
 
-// FIXME
-const etag = "some-hash";
+const sha1 = async (message: string) => {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(message);
+  const hash = await crypto.subtle.digest("SHA-1", data);
+
+  const hashArray = Array.from(new Uint8Array(hash));
+  return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+};
 
 const app = new Hono();
 app.get("*", async (c, next) => {
-  const ifNoneMatch = c.req.header("If-None-Match");
-  if (ifNoneMatch === etag)
-    return new Response(null, {
-      status: 304,
-      statusText: "Not Modified",
-      headers: {
-        ETag: etag,
-      },
-    });
+  let etag: string = "";
+  if (c.env?.__STATIC_CONTENT_MANIFEST) {
+    etag = await sha1(btoa(JSON.stringify(c.env.__STATIC_CONTENT_MANIFEST)));
+    const ifNoneMatch = c.req.header("If-None-Match");
+    if (ifNoneMatch === etag)
+      return new Response(null, {
+        status: 304,
+        statusText: "Not Modified",
+        headers: {
+          ETag: etag,
+        },
+      });
+  }
 
   await next();
-  c.res.headers.set("ETag", etag);
+  etag && c.res.headers.set("ETag", etag);
 });
 app.get("*", qwikMiddleware({ render, qwikCityPlan }));
 
