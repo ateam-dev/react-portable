23c7e50701148abdbf2deb7b2c5774ef06c2635d
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-07-11 18:00:00 +0900

    update example for nextjs

---
+++ examples/nextjs/src/pages/pokemon.tsx
@@ -0,0 +1,33 @@
+import React, { useReducer } from "react";
+import useSWR from "swr";
+import { Pokemon } from "examples-tailwind-ui";
+
+type PokemonData = { imgSrc: string; name: string; types: string[] };
+
+const getPokemonData = async (id: number): Promise<PokemonData> => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return {
+      imgSrc: data.sprites.front_default,
+      name: data.name,
+      types: data.types.map(
+        ({ type }: { type: { name: string } }) => type.name
+      ),
+    };
+  } catch (e) {
+    return { imgSrc: "", name: "", types: [] };
+  }
+};
+
+export default function Home() {
+  const [id, increment] = useReducer((s: number) => s + 1, 1);
+
+  const { data: pokemon } = useSWR(id.toString(), getPokemonData, {
+    fallbackData: { imgSrc: "", name: "", types: [] },
+    keepPreviousData: true,
+  });
+
+  return <div onClick={increment}>{pokemon && <Pokemon {...pokemon} />}</div>;
+}
