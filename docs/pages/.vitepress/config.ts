802e1dc36613a2172453ef66ac3f5923a6c00072
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-08-28 12:40:27 +0900

    update doc for og image and favicon

--- docs/pages/.vitepress/config.ts
+++ docs/pages/.vitepress/config.ts
@@ -3,7 +3,18 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: "/react-portable/",
-  title: "♻️ Previewify - React Portable",
+  title: "Previewify - React Portable",
+  head: [
+    ["meta", { property: "og:image", content: "/react-portable/ogp.png" }],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "https://cdn.jsdelivr.net/gh/twitter/twemoji/assets/72x72/267b.png",
+      },
+    ],
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     siteTitle: "♻️ Previewify",
