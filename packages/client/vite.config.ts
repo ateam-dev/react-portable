96ca1030c2962ba176104ece38a06ae7249fce44
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-09-08 21:00:00 +0900

    remove unnecessary dependencies and optimize build tasks

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/browser.ts",
+      formats: ["umd"],
+      name: "ReactPortable",
+      fileName: "browser",
+    },
+    emptyOutDir: false,
+  },
   test: {
     environment: "jsdom",
     environmentOptions: {
---
+++ packages/core/build.config.ts
@@ -0,0 +1,39 @@
+import { defineBuildConfig } from "unbuild";
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+export default defineBuildConfig({
+  hooks: {
+    "rollup:options"(ctx, options) {
+      if (Array.isArray(options.plugins)) {
+        options.plugins.unshift({
+          name: "raw-loader",
+          resolveId(source, importer) {
+            if (source.endsWith("?raw")) {
+              return source.startsWith(".")
+                ? path.resolve(path.dirname(importer), source)
+                : source;
+            }
+            return null;
+          },
+          load(id) {
+            if (id.endsWith("?raw")) {
+              const content = fs.readFileSync(
+                require.resolve(id.replace(/\?raw$/, "")),
+                "utf-8",
+              );
+              return `export default ${JSON.stringify(content)};`;
+            }
+            return null;
+          },
+        });
+      }
+    },
+  },
+  rollup: {
+    output: {
+      exports: "named",
+    },
+    inlineDependencies: true,
+  },
+});
